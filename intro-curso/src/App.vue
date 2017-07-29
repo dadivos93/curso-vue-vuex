a52b28582e7fffbd5318314ddae0438111c6faf7
davivos93 <davidj.garcastillo@gmail.com>
2017-07-29 08:19:00 -0600

    Nuestro primer componente

--- intro-curso/src/App.vue
+++ intro-curso/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <intro></intro>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Intro from './components/Intro'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    Intro
   }
 }
 </script>
---
+++ intro-curso/src/components/Intro.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="intro">
+    <h1>Intro Component</h1>
+    <p>{{ txt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      txt: 'Hola mundo desde Intro'
+    }
+  }
+}
+</script>
+
